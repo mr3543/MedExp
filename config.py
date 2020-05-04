63c86805e1e651d5deed383e2b30c0e8fb453b8e
mmr <mr3543@columbia.edu>
2020-05-03 22:34:03 -0400

    change default number of links on page

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     def __init__(self):
         self.db_file = 'med_db.db'
         self.xfile = './data/X.pkl'
-        self.num_articles_home = 20
+        self.num_articles_home = 100
         self.num_search_results = 100
-        self.num_recs = 20
+        self.num_recs = 100
         self.data_path = './data/Medium_AggregatedData.csv'
--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, \
+from flask import Flask, 
                   render_template, redirect, flash, session, Response
 from time import sleep
 from werkzeug.security import check_password_hash, \
